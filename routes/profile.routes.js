b3c3b7b1757c34c969262b2dde70e94fbf8af91c
Keifer Shearer <keifearer@gmail.com>
2023-03-09 14:21:39 +0100

    created profile route

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const FreePost = require('../models/FreePost.model.js')
+const User = require('../models/User.model')
 
+router.get('/', async (req, res, next) => {
+    try {
+        const myPost = await FreePost.find({ owner: req.session.currentUser._id })
+        const user = await User.findById(req.session.currentUser._id)
+        res.json('profile', { user, myPost })
+    } catch (err) {
+        next(err)
+    }
+})
 
 
 
