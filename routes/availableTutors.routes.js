decb64d798352b5be0a6435287d418dbc755abf2
TomMahe <mahettom@gmail.com>
2023-03-11 14:18:05 +0100

    test postman of first route

--- routes/availableTutors.routes.js
+++ routes/availableTutors.routes.js
@@ -1,7 +1,17 @@
 const express = require('express');
+const User = require('../models/User.model');
 const router = express.Router();
 
 
+router.get('/', async (req, res, next) => {
+    try {
+        const tutors = await User.find({ isTutor: true })
+        console.log(tutors)
+        res.json(tutors)
+    } catch (error) {
+        next(error)
+    }
+})
 
 
 
