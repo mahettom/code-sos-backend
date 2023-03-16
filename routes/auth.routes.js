1eec8b4253d6e7cfa7eff5c482fec3e15076c94d
Keifer Shearer <keifearer@gmail.com>
2023-03-16 19:08:14 +0100

    regex

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -18,11 +18,11 @@ router.post('/signup', fileUpload.single('profilePic'), (req, res, next) => {
         res.status(400).json({ message: "Provide email, password, username" })
         return
     }
-    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
-    // if (!emailRegex.test(email)) {
-    //     res.status(400).json({ message: 'Provide a vaild email address' })
-    //     return
-    // }
+    const emailRegex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (!emailRegex.test(email)) {
+        res.status(400).json({ message: 'Provide a vaild email address' })
+        return
+    }
     const passwordRegex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
     if (!passwordRegex.test(password)) {
         res.status(400).json({ message: 'Password must have at least 6 characters and contain at least one number, one lowercase and one uppercase letter.' })
